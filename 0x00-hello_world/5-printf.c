1d1850f2c4cab0b72f4cb49ed7da304bc343ec1e
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-08 11:34:40 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *t5 = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", t5);
+	return (0);
+}
