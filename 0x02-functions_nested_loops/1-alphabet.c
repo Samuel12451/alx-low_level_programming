1b39e5c64a913044aa784d90dbb192b2cdb653e6
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-13 13:42:02 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print alphabet, in lowercase
+ *
+ * Description: print alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+
+}
