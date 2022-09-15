5c57b2f10acf621ea53eb91a6c779155d73549de
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-15 14:42:26 -0700

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal line
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int l = 0;
+	int s;
+
+	if (n > 0)
+	{
+		while (l < n)
+		{
+			for (s = 0; s < l; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			l++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
