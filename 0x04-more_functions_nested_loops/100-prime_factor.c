72c44bb97fbcc63a6537223000a1b0dc9f98d7e6
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-15 14:57:13 -0700

    task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+i#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int p = 612852475143;
+	long int q = p;
+	long int d = 2;
+
+	while (q != d)
+	{
+		if (q % d == 0)
+		{
+			q = q / d;
+		}
+		else
+		{
+			d++;
+		}
+	}
+
+	printf("%li\n", q);
+
+	return (0);
+}
