cc4be57c7f67590b15a3cdebaae283bac41f1742
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-19 07:49:07 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @st: string
+ * Return: void
+ */
+
+void _puts(char *st)
+{
+	int i = 0;
+
+	while (st[i] != '\0')
+	{
+		_putchar(st[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
