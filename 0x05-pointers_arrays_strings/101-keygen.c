1426fecc81e02f8906eb47d1f03fb3f4248f7475
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-19 13:08:19 -0700

    last chance

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,25 +13,25 @@ void randomPasswordGeneration(int N)
 {
 
 	int i = 0;
-  
+
 	int randomizer = 0;
-  
-	srand((unsigned int)(time(NULL)))
+
+	srand((unsigned int)(time(NULL)));
 
 	char numbers[] = "0123456789";
- 
+
 	char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
+
 	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
 
 	char symbols[] = "!@#$^&*?";
- 
+
 	char password[N];
-  
+
 	randomizer = rand() % 4;
-  
+
 	for (i = 0; i < N; i++) {
-  
+
 	if (randomizer == 1) {
 		password[i] = numbers[rand() % 10];
 		randomizer = rand() % 4;
