43d94551bcc29888a6f9de18d6d1747e7afd8757
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-19 11:01:53 -0700

    task 08

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,20 +10,11 @@
 
 void print_array(int *a, int n)
 {
-	int i, count;
+	int i;
 
-	i = 0;
-	count = 1;
-	if (n <= 0)
-		n = 0;
-	if (n > 0)
-	{
-		while (a[i] != '\0' && count < n)
-		{
-			printf("%d, ", a[i]);
-			i++;
-			count++;
-		}
-		printf("%d\n", a[i]);
-	}
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, ", a[i]);
+	if (i == (n - 1))
+		printf("%d", a[n - 1]);
+	printf("\n");
 }
