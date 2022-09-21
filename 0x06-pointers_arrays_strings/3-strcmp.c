707dd3288518592240defbde03da90eab3f63c3b
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-21 11:33:06 -0700

    task done 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strcmp - Compare to string
+ * _strcmp - Compares two strings.
  * @s1: first pointer
  * @s2: second pointer
  * Return: string
  */
 int _strcmp(char *s1, char *s2);
 {
-	int count, varcom;
+	int counter, varcom;
 
-	count = 0;
+	counter = 0;
 
-	while (s1[count] == s2[count] && s1[count] != '\0')
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
 	{
-		count++;
+		counter++;
 	}
-	varcom = s1[count] - s2[count];
+	varcom = s1[counter] - s2[counter];
 
 	return (varcom);
 
