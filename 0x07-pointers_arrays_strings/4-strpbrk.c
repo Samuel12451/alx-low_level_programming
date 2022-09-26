83dc922739881dbbad378ab45e99e4d199bd02f6
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-26 11:57:33 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,27 +1,27 @@
 #include "main.h"
-#define NULL 0
+#include <stdio.h>
 
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: string
- * @accept: chars to compare
- * Return: pointer from the initial segment of match
+ * _strpbrk - bytes
+ * @s: pointer
+ * @accept: pointer
+ * Return: null
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	int a, b;
+	int a;
 
-	for (a = 0; *(s + a) != '\0'; a++)
+	while (*s)
 	{
-		for (b = 0; *(accept + b) != '\0'; b++)
+		for (a = 0; accept[a]; a++)
 		{
-			if (*(s + a) == *(accept + b))
+			if (*s == accept[a])
 			{
-				return (s + a);
+				return (s);
 			}
 		}
+		s++;
 	}
-
 	return (NULL);
 }
