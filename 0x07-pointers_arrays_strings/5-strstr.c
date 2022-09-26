5eee6b4456e74662139e8a766f5019a7fae1189e
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-26 12:06:17 -0700

    task 05

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,38 +1,32 @@
 #include "main.h"
 
 /**
- * _strstr - locates substring
- * @haystack: search
- * @needle: match
- * Return: pointer
- */
+* _strstr - function locate
+* @haystack: pointer
+* @needle: pointer
+* Return: 0
+*/
 
 char *_strstr(char *haystack, char *needle)
 {
-	int a, b, start;
-	int nlen = 0;
+	char *res = haystack, *fneedle = needle;
 
-	a = 0;
-	b = 0;
-	start = 0;
-
-	while (needle[nlen] != '\0')
+	while (*haystack)
 	{
-		nlen++;
-	}
-	for (a = 0; haystack[a] != '\0'; a++)
-	{
-		for (b = 0; b < nlen && haystack[a] == needle[b]; b++, a++)
+		while (*needle)
 		{
-			if (b == 0)
+			if (*haystack++ != *needle++)
 			{
-				start = a;
-			}
-			if (b == nlen - 1)
-			{
-				return (haystack + start);
+				break;
 			}
 		}
+		if (!*needle)
+		{
+			return (res);
+		}
+		needle = fneedle;
+		res++;
+		haystack = res;
 	}
 	return (0);
 }
