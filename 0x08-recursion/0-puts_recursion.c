a2bcaba1396b82ec5d1209fd13565bcab892ce12
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-28 13:01:04 -0700

    task oo

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	
-		putchar('\n');
-	
+	if (*s != '\0')
+	{
+		_putchar(*(s + 0));
+		_puts_recursion((s + 1));
+	}
 	else
 	{
-		putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
 }
