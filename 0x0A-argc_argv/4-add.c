0810fd80d017290e4e2dbbf9c9547dec38ea6fd0
samuel12451 <samuel.tesema.st@gmail.com>
2022-09-30 06:53:03 -0700

    task 04

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,66 +1,28 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
- * is_number - checks to see if input is a number
- * @s: input to check for numberhood
- * Return: 1 if it is a number, 0 if not
+ * main - add positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
-int is_number(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*(s + i) != '\0')
-	{
-		if (*(s + i) >= '0' && *(s + i) <= '9')
-		{
-			i++;
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
-
-/**
- * main - prints the sum of positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: (0)
- */
-
 int main(int argc, char *argv[])
 {
-	int i, sum, is_num;
+	int a, b, add = 0;
 
-	sum = 0;
-	if (argc == 1)
+	for (a = 1; a < argc; a++)
 	{
-		printf("0\n");
-	}
-	else if (argc > 1)
-	{
-		i = 1;
-		while (i < argc)
+		for (b = 0; argv[a][b] != '\0'; b++)
 		{
-			is_num = is_number(argv[i]);
-			if (is_num == 1)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
+			if (!isdigit(argv[a][b]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			i++;
 		}
-		printf("%d\n", sum);
+		add += atoi(argv[a]);
 	}
+	printf("%d\n", add);
 	return (0);
 }
