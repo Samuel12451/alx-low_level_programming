34b556710c7e5dda52c8cc4de4724671694178d2
samuel12451 <samuel.tesema.st@gmail.com>
2022-10-20 11:07:40 -0700

    doner

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,15 @@
  */
 size_t print_list(const list_t *h)
 {
-  size_t node_count = 0;
+	size_t node_count = 0;
 
-  for ( ; h != NULL; h = h->next)
-  {
-    if (h->str == NULL)
-      printf("[0] (nil)\n");
-    else
-      printf("[%i] %s\n", h->len, h->str);
-    node_count += 1;
-  }
-  return (node_count);
+	for ( ; h != NULL; h = h->next)
+	{
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%i] %s\n", h->len, h->str);
+		node_count += 1;
+	}
+	return (node_count);
 }
